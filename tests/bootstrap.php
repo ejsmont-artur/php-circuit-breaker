2dc67a1fae9067f3abde2d64dfbd782b4f97cc2c
aejsmont <ejsmont.artur@gmail.com>
2013-02-23 18:27:25 +1100

    more travisci debugging

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,5 @@
 <?php
 
-// for some reason travisci was showing that apc is loaded but function_exists('apc_clear_cache') would still fail
-if (!extension_loaded('apc')) {
-    dl('apc.so');
-}
-
 require dirname(__FILE__).'/SplClassLoader.php';
 
 $autoLoader = new SplClassLoader('Ejsmont', dirname(__FILE__).'/../src');
