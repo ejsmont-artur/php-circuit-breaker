8dec1005e8b4ac78130aeaaf2d2db4bdc24fd31b
Dick <dick@insided.nl>
2016-02-04 21:01:12 +0100

    removed duplication and fixed undefined class error in docblock

--- src/Ejsmont/CircuitBreaker/Storage/Decorator/ArrayDecorator.php
+++ src/Ejsmont/CircuitBreaker/Storage/Decorator/ArrayDecorator.php
@@ -2,21 +2,7 @@
 
 /**
  * This file is part of the php-circuit-breaker package.
- * 
- * @link https://github.com/ejsmont-artur/php-circuit-breaker
- * @link http://artur.ejsmont.org/blog/circuit-breaker
- * @author Artur Ejsmont
  *
- * For the full copyright and license information, please view the LICENSE file.
- */
-
-namespace Ejsmont\CircuitBreaker\Storage\Decorator;
-
-use Ejsmont\CircuitBreaker\Storage\StorageInterface;
-
-/**
- * This file is part of the php-circuit-breaker package.
- * 
  * @link https://github.com/ejsmont-artur/php-circuit-breaker
  * @link http://artur.ejsmont.org/blog/circuit-breaker
  * @author Artur Ejsmont
@@ -35,7 +21,7 @@ use Ejsmont\CircuitBreaker\Storage\StorageException;
  * Otherwise every service counter/time would have to be loaded separately.
  * Decorator will group updates until flush is requested.
  * On save flush decorator will load stats again and update them with new values.
- * 
+ *
  * @see Ejsmont\CircuitBreaker\Storage\StorageInterface
  * @package Ejsmont\CircuitBreaker\Components
  */
@@ -68,7 +54,7 @@ class ArrayDecorator implements StorageInterface {
 
     /**
      * Configure decorator instance
-     * 
+     *
      * @param StorageInterface $wrappedInstance instance of storage interface that will be used
      */
     public function __construct(StorageInterface $wrappedInstance) {
@@ -78,17 +64,17 @@ class ArrayDecorator implements StorageInterface {
     /**
      * Loads circuit breaker service status values from array.
      * Only one remote call for all services.
-     * 
+     *
      * @param 	string  $serviceName   name of service to load stats for
      * @param 	string  $attributeName name of attribute to load
      * @return 	string  value stored or '' if value was not found
-     *  
-     * @throws Ejsmont\CircuitBreaker\Storage\StorageException if storage error occurs, handler can not be used
+     *
+     * @throws StorageException if storage error occurs, handler can not be used
      */
     public function loadStatus($serviceName, $attributeName) {
         // make sure we have the values loaded (request time cache of all service stats)
         $this->loadStatsArray();
-        // return 
+        // return
         if (isset($this->stats[$serviceName][$attributeName])) {
             return $this->stats[$serviceName][$attributeName];
         } else {
@@ -100,14 +86,14 @@ class ArrayDecorator implements StorageInterface {
      * We reload values just before saving to reduce race condition time.
      * The updated values are merged with previous values.
      * Then everything is stored back into the storage
-     * 
+     *
      * @param 	string  $serviceName   name of service to load stats for
-     * @param 	string  $attributeName name of the attribute to load 
-     * @param 	string  $value         string value loaded or '' if nothing found 
+     * @param 	string  $attributeName name of the attribute to load
+     * @param 	string  $value         string value loaded or '' if nothing found
      * @param   boolean $flush         set to true will force immediate save, false does not guaranteed saving at all.
      * @return 	void
-     * 
-     * @throws Ejsmont\CircuitBreaker\Storage\StorageException if storage error occurs, handler can not be used
+     *
+     * @throws StorageException if storage error occurs, handler can not be used
      */
     public function saveStatus($serviceName, $attributeName, $value, $flush = false) {
         // before writing we load the data no matter what
@@ -140,7 +126,7 @@ class ArrayDecorator implements StorageInterface {
     /**
      * Method that actually loads the stats array from wrapped instance.
      * Loads stats only if need to be loaded.
-     * 
+     *
      * @return void
      */
     protected function loadStatsArray() {
@@ -160,7 +146,7 @@ class ArrayDecorator implements StorageInterface {
     /**
      * Method that actually saves the stats array in wrapped instance.
      * Saves only if there is dirty service data.
-     * 
+     *
      * @return void
      */
     protected function saveStatsArray() {
